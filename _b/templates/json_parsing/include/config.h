556b49079a5d5b71e9eacced4c028714db14423b
Nico398 <122436148+Nico398@users.noreply.github.com>
2023-10-20 15:24:45 +0200

    Fehlerbehebung

---
+++ _b/templates/json_parsing/include/config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H_IN
+#define CONFIG_H_IN
+
+constexpr auto PROJECT_NAME {"json_parsing"};
+constexpr auto PROJECT_VER  {"0.0.1"};
+constexpr auto PROJECT_VER_MAJOR {"0"};
+constexpr auto PROJECT_VER_MINOR {"0"};
+constexpr auto PTOJECT_VER_PATCH {"1"};
+constexpr auto PROJECT_BUILD_DATE {"2023-10-20T14:46:39"};
+
+#endif // CONFIG_H_IN
