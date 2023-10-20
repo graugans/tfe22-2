1fc0fc066562016bd56b39d00eace833d160fdda
lars250 <122730128+lars250@users.noreply.github.com>
2023-10-20 15:25:26 +0200

    Add Option -c --count

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -11,9 +11,12 @@ auto main(int argc, char **argv) -> int
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
     CLI::App app{PROJECT_NAME};
+    int count = 20;
+
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count", count, "Eingabe des Parameters.");
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -27,6 +30,7 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", app.get_name());
+    fmt::print("Counter: {}\n", count);
 
     /* INSERT YOUR CODE HERE */
 
