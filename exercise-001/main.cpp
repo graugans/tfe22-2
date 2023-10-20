b55f916c7e1b439be57d4863a4b9efb0565bcf68
lucadiener <122467797+lucadiener@users.noreply.github.com>
2023-10-20 15:37:35 +0200

    Aufgabe 1:Parameterraum

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -10,16 +10,19 @@ auto main(int argc, char **argv) -> int
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
+     int count = 20;
     CLI::App app{PROJECT_NAME};
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
         app.parse(argc, argv);
+        app.add_option("-c,--count",count,"Anzahl festlegen")->check(CLI::Range(0,100));
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
+
 
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
@@ -29,6 +32,9 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}!\n", app.get_name());
 
     /* INSERT YOUR CODE HERE */
+   
+    fmt::print("Wert: {}\n",count);
+
 
     return 0; /* exit gracefully*/
 }
