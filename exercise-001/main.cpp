7b11be87005d92e15a610d020f3605b21101f60c
DennisDick <122461822+DennisDick@users.noreply.github.com>
2023-10-20 15:37:39 +0200

    First push

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -6,20 +6,25 @@
 
 auto main(int argc, char **argv) -> int
 {
+    auto count = 20;
+
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
+
     CLI::App app{PROJECT_NAME};
     try
     {
+        app.add_option("-c, --count", count, "set count:") ->check(CLI::Range(0, 100));
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
-    }
+    }    
+    
 
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
@@ -27,8 +32,11 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", app.get_name());
+    fmt::print("Count: {}\n", count);
 
     /* INSERT YOUR CODE HERE */
+
+
 
     return 0; /* exit gracefully*/
 }
