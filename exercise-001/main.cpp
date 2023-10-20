479d150ce825f413db6f73188cff3179929d204e
SunilRednael <122459030+SunilRednael@users.noreply.github.com>
2023-10-20 16:26:14 +0200

    added random bibliothek

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -1,22 +1,20 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
+#include <random>  // Für Zufallszahlen
+#include <vector>
 
 #include "CLI/CLI.hpp"
 #include "config.h.in"
 
 auto main(int argc, char **argv) -> int
 {
-    /**
-     * CLI11 is a command line parser to add command line options
-     * More info at https://github.com/CLIUtils/CLI11#usage
-     */
     CLI::App app{PROJECT_NAME};
 
-    int count = 20; //Standardwert
+    int count = 20; // Standardwert
 
     try
     {
-     app.add_option("-c,--count", count, "set count;") -> check(CLI::Range(0,100));
+        app.add_option("-c,--count", count, "set count;")->check(CLI::Range(0, 100));
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
         app.parse(argc, argv);
     }
@@ -25,15 +23,26 @@ auto main(int argc, char **argv) -> int
         return app.exit(e);
     }
 
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
+    // Erzeugen Sie einen Zufallszahlengenerator
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(1, 100);
+
+    // Erstellen Sie einen std::vector mit zufälligen Werten
+    std::vector<int> random_values(count);
+    for (int &value : random_values) {
+        value = dist(gen);
+    }
+
     fmt::print("Hello, {}!\n", app.get_name());
-    fmt::print("count: {}\n, count", count);
+    fmt::print("count: {}\n", count);
 
-    /* INSERT YOUR CODE HERE */
+    // Ausgabe der zufälligen Werte
+    fmt::print("Zufällige Werte:");
+    for (int value : random_values) {
+        fmt::print(" {}", value);
+    }
+    fmt::print("\n");
 
-    return 0; /* exit gracefully*/
+    return 0; /* graceful exit */
 }
