1cbb20ccb060d27f51b271103204ebe040a71612
Conrad <122440196+cndrw@users.noreply.github.com>
2023-10-20 16:09:46 +0200

    added sorting the vector and measuring its time

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -7,9 +7,15 @@
 #include <vector>
 #include <cstdlib>
 
+auto print_vector(const std::vector<int32_t>& vec) -> void
+{
+    fmt::print("{}\n", fmt::join(vec, ", "));
+}
+
 auto main(int argc, char **argv) -> int
 {
     int32_t count = 20;
+    std::vector<int32_t> rand_numbers;
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -21,7 +27,6 @@ auto main(int argc, char **argv) -> int
         app.add_option<int32_t>("-c, --count", count);
         app.parse(argc, argv);
 
-        std::vector<int32_t> rand_numbers;
         srand((unsigned) time(NULL));
 
         for (int32_t i = 0; i < count; i++)
@@ -40,6 +45,18 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", app.get_name());
+    std::cout << "raw: ";
+    print_vector(rand_numbers);
+
+    auto start = std::chrono::system_clock::now();
+    std::sort(rand_numbers.begin(), rand_numbers.end());
+    auto end = std::chrono::system_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "sorted in : ";
+    print_vector(rand_numbers);
+    fmt::print("sorted in {}\n", elapsed);
+
+
 
     /* INSERT YOUR CODE HERE */
 
