2d7276847f1e9eb25365a24e056a57e7217f5331
Simon <122939588+SimonICEMAN@users.noreply.github.com>
2023-10-20 15:42:46 +0200

    Aufgabe 1.1: Vektorerstellung

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -1,5 +1,7 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
+#include <vector>
+#include <random>
 
 #include "CLI/CLI.hpp"
 #include "config.h"
@@ -7,15 +9,12 @@
 auto main(int argc, char **argv) -> int
 {
     CLI::App app{PROJECT_NAME};
-    int count = 20; // Standardwert
+    int count = 20;
 
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-        
-        // Füge eine Option für "count" hinzu
         app.add_option("-c,--count", count, "Anzahl festlegen")->check(CLI::Range(0, 100));
-
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -24,7 +23,22 @@ auto main(int argc, char **argv) -> int
     }
 
     fmt::print("Hello, {}!\n", app.get_name());
-    fmt::print("Count: {}\n", count); // Gib den Wert von "count" aus
+
+    // Erstelle einen std::vector mit zufälligen Werten von 1-100
+    std::vector<int> random_values;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(1, 100);
+
+    for (int i = 0; i < count; ++i) {
+        random_values.push_back(dis(gen));
+    }
+
+    fmt::print("Random Values: ");
+    for (int value : random_values) {
+        fmt::print("{} ", value);
+    }
+    fmt::print("\n");
 
     return 0;
 }
