26cd92b40bfaeede31ce9a28a16b078a54970ef6
Max-LKn <122216817+Max-LKn@users.noreply.github.com>
2023-10-20 15:26:47 +0200

    Task Parameterraum

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -10,10 +10,13 @@ auto main(int argc, char **argv) -> int
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
+    int count = 20;
+
     CLI::App app{PROJECT_NAME};
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count", count, "Enter the number of vectors you want to create");
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -29,6 +32,7 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}!\n", app.get_name());
 
     /* INSERT YOUR CODE HERE */
+    fmt::print("Count: {}\n", count);
 
     return 0; /* exit gracefully*/
 }
