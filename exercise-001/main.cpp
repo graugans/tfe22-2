af11a81d0fd7374df1e387cbf9ce4c0aa1ccf5b3
DiepLau <lauradiep68@gmail.com>
2023-10-20 16:14:46 +0200

    Random Exercise
    Created a vector with random values ranging from 0 to 100

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -1,11 +1,13 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
 
+#include <random>
 #include "CLI/CLI.hpp"
 #include "config.h"
 
 auto main(int argc, char **argv) -> int
 {
+    auto count = 20;
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -14,8 +16,7 @@ auto main(int argc, char **argv) -> int
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-        std::int8_t count = 20;
-        app.add_option("-c, --count", count, "default");
+        app.add_option("-c, --count", count, fmt::format("The default is:{}",count));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
@@ -30,6 +31,14 @@ auto main(int argc, char **argv) -> int
      */
     fmt::print("Hello, {}!\n", app.get_name());
     
+
+
+    // Initialize random time seed 
+    srand ( time(NULL) );   
+
+    std::vector<uint32_t> vec_random;
+    for (int i = 0; i <= 20; i++)
+        vec_random.push_back(rand() % 100 + 1); // Add a random number between 1 and 100
 
     /* INSERT YOUR CODE HERE */
 
