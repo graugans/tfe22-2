79125b947a64fff36c7ca78127f3681120ab5191
Christian Ege <ch@ege.io>
2023-12-04 14:10:57 +0100

    Safe exercise-006

--- exercise-006/main.cpp
+++ exercise-006/main.cpp
@@ -7,37 +7,58 @@
 class Tier
 {
    public:
-    void iss()
+    virtual void iss() const = 0;
+    /*
+    da iss pure virtual ist
     {
         fmt::println("Fresse wie ein Tier");
     };
+    */
 };
 class Hund : public Tier
 {
    public:
-    void iss()
+    void iss() const final
     {
         fmt::println("Wuff! Fresse gerade");
     };
 };
+class Pudel : public Hund
+{
+   public:
+    /*
+    because iss is marked as final in Hund
+     void iss() const override
+     {
+         fmt::println("Der Pudel frisst gerade");
+     };
+     */
+};
 class Mensch : public Tier
 {
    public:
-    void iss()
+    void iss() const override
     {
         fmt::println("Esse gerade");
     };
 };
 
+void tierPrinter(Tier* t)
+{
+    t->iss();
+}
+
 int main()
 {
     std::vector<Tier*> tiere;
-    tiere.push_back(new Tier());
+    // tiere.push_back(new Tier());
     tiere.push_back(new Hund());
+    tiere.push_back(new Pudel());
     tiere.push_back(new Mensch());
-    for (auto tier: tiere)
+    for (auto* tier : tiere)
     {
         tier->iss();
     }
+
     return 0;
 }
